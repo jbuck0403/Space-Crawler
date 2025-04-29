4bca3bb7961c8d3fd483f244004d1a0210bc32b1
Jack Buck <89940883+jbuck0403@users.noreply.github.com>
2025-04-29 13:56:26 -0700

    reorganized file structure

--- Assets/_Project/Scripts/Runtime/Core/Combat/Weapons/ProjectileFiring/Legacy/Strategies/SniperFiringStrategy.cs
+++
@@ -1,7 +0,0 @@
-// using UnityEngine;
-
-// [CreateAssetMenu(
-//     fileName = "SniperFiringStrategy",
-//     menuName = "SpaceShooter/Strategies/Firing Strategies/Sniper"
-// )]
-// public class SniperFiringStrategy : BaseFiringStrategy { }
